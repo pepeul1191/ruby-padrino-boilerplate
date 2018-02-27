18460f03b4fba7260b8cca972aee45f8b2891118
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-26 20:58:12 -0500

    Renderizando primera vista

---
+++ app/controllers/login.rb
@@ -0,0 +1,38 @@
+App::App.controllers :login do
+
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+
+  get :index do
+    render 'login/index', :layout => 'blank'
+  end
+
+  post :acceder do
+
+  end
+
+  get :ver_usuario do
+
+  end
+
+  get :salir do
+
+  end
+
+end
