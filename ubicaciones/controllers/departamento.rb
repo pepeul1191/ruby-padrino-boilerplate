f1e58af7a19cf55abd1f33f072578f70e2490f14
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-26 21:44:31 -0500

    Renderizado de archivos estáticos con helper

--- ubicaciones/controllers/departamento.rb
+++ ubicaciones/controllers/departamento.rb
@@ -2,7 +2,18 @@ require_relative '../models/departamento'
 
 App::Ubicaciones.controllers :departamento do
   get :index do
-    render 'departamento/index', :layout => 'blank'
+    locals = {
+      :csss => [
+        'bower_components/bootstrap/dist/css/bootstrap.min',
+        'bower_components/font-awesome/css/font-awesome.min',
+      ],
+      :jss => [
+        'bower_components/jquery/dist/jquery.min',
+        'bower_components/bootstrap/dist/js/bootstrap.min',
+        'bower_components/requirejs/require',
+      ],
+    }
+    render 'departamento/index', :layout => 'blank', :locals => locals
   end
 
   get :listar do
